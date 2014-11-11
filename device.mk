c259e4f609933a94dcccd5a5756f6603767a4d18
joohyoung Yang <yangjoo@debian.yangjoo.net>
2014-11-11 13:33:47 +0900

    modify ring delay

--- device.mk
+++ device.mk
@@ -92,7 +92,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungExynos4RIL \
     mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
     ro.telephony.call_ring.multiple=false \
-    ro.telephony.call_ring.delay=3000
+    ro.telephony.call_ring.delay=1000
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
