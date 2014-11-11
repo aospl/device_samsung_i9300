c13a0afd32ea538a96948a5f91c445a72ff85d18
joohyoung Yang <yangjoo@debian.yangjoo.net>
2014-11-11 13:32:38 +0900

    Add ramdisk fstab for M440S

--- device.mk
+++ device.mk
@@ -32,6 +32,7 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 # Init files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.smdk4x12:root/fstab.smdk4x12 \
+    $(LOCAL_PATH)/rootdir/fstab.SHW-M440S:root/fstab.SHW-M440S \
     $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc
 
 # Audio
